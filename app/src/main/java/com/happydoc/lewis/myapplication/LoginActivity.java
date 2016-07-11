68371b3c7736aced1c49f3779b6440310a542883
lewis <lewis199328@gmail.com>
2016-07-11 17:02:35 +0800

    happy doctor first major change!

---
+++ app/src/main/java/com/happydoc/lewis/myapplication/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.happydoc.lewis.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lewis on 2016/7/11.
+ */
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
